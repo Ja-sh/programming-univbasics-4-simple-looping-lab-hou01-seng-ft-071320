ab691b23d4efac925a459d33270b82c3a62c853c
Joshua Moore <morejoshua99@hotmail.com>
2020-05-21 22:31:01 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -7,3 +7,14 @@ while counter < 5 do
   counter += 1
   end
 end
+
+
+def loop_message_n_times(message)
+  counter = 0
+
+while counter 0 < message.length do
+  puts message
+  counter += 1 
+  end
+end
+  
